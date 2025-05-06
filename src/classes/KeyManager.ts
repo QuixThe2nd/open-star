f9efdee3001adc4d4b72caf4ec7322d41ce5d2cc
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-06 16:38:45 +1000

    Store mnemonic as raw string instead of json

--- src/classes/KeyManager.ts
+++ src/classes/KeyManager.ts
@@ -6,34 +6,39 @@ import { keccak256, verifyMessage } from 'viem/utils';
 import { wordlist } from '@scure/bip39/wordlists/english';
 
 export class KeyManager {
-  private account: PrivateKeyAccount;
+  private readonly account: PrivateKeyAccount;
   readonly id: string
   private readonly privateKey: `0x${string}`
 
   private constructor(id: string | number, mnemonic: string) {
     this.id = String(id)
     const seed = mnemonicToSeedSync(mnemonic)
-    const privateKeyBytes = HDKey.fromMasterSeed(seed).privateKey!
+    const privateKeyBytes = HDKey.fromMasterSeed(seed).privateKey
+    if (privateKeyBytes === null) throw new Error('Failed to get private key')
     this.privateKey = `0x${Array.from(privateKeyBytes).map(b => b.toString(16).padStart(2, '0')).join('')}`
     this.account = privateKeyToAccount(this.privateKey);
   }
 
-  static init = async (id: string | number = '') => {
-    if (!id) id = Math.random()
+  static init = async (id?: string | number) => {
+    id ??= Math.random()
     const fs = typeof window === 'undefined' ? await import('fs') : undefined
 
-    const keyFile = `keyPair_${id}.json`
-    if (fs && fs.existsSync(keyFile)) return new KeyManager(id, (JSON.parse(fs.readFileSync(keyFile, 'utf-8')) as { mnemonic: string }).mnemonic)
-    else if (typeof localStorage !== 'undefined' && localStorage.getItem(keyFile) !== null) return new KeyManager(id, localStorage.getItem(keyFile)!)
-    else {
-      const mnemonic = generateMnemonic(wordlist)
-      if (typeof localStorage !== 'undefined') localStorage.setItem(keyFile, mnemonic)
-      else if (typeof fs !== 'undefined') fs.writeFileSync(keyFile, JSON.stringify({ mnemonic }, null, 2))
-      return new KeyManager(id, mnemonic)
+    const keyFile = `keyPair_${id}.txt`
+    if ((fs?.existsSync(keyFile)) === true) return new KeyManager(id, fs.readFileSync(keyFile, 'utf-8'))
+    if (typeof localStorage !== 'undefined') {
+      const localStorageItem = localStorage.getItem(keyFile)
+      if (localStorageItem !== null) return new KeyManager(id, localStorageItem)
     }
+
+    const mnemonic = generateMnemonic(wordlist)
+    if (typeof localStorage !== 'undefined') localStorage.setItem(keyFile, mnemonic)
+    else if (typeof fs !== 'undefined') fs.writeFileSync(keyFile, mnemonic)
+    return new KeyManager(id, mnemonic)
   }
 
-  sign = (message: string): Promise<`0x${string}`> => sign({ hash: keccak256(toPrefixedMessage(message), 'hex'), privateKey: this.privateKey, to: 'hex' })
-  verify = (signature: `0x${string}`, message: string, address: `0x${string}`): Promise<boolean> => verifyMessage({ address, message, signature });
-  getPublicKey = () => this.account.address;
+  sign = async (message: string): Promise<`0x${string}`> => sign({ hash: keccak256(toPrefixedMessage(message), 'hex'), privateKey: this.privateKey, to: 'hex' })
+  verify = async (signature: `0x${string}`, message: string, address: `0x${string}`): Promise<boolean> => verifyMessage({ address, message, signature });
+  get address(){
+    return this.account.address;
+  }
 }
