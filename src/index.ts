fae3f8d390ef0e5a85b647a64f477a29d7acac67
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-07 15:36:22 +1000

    export ORC20 Oracle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { KeyManager } from './classes/KeyManager'
 export { start } from './oracle'
 export { OpenStar } from './oracle/OpenStar'
+export { ORC20Oracle } from './oracle/ORC20';
 export type * from './types/Oracle'
 export type * from './types/ORC20'
 export { StateManager } from './classes/StateManager'
