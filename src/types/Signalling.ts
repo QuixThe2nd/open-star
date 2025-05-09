803bdc04f31e0bd80d6ce74ef8de7e867a3809d2
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-09 14:04:04 +1000

    Remove unneeded type

--- src/types/Signalling.ts
+++ src/types/Signalling.ts
@@ -3,5 +3,3 @@ export type RTCObjectType = {
 }
 
 export type SignallingMessage = { announce: true; from: `0x${string}` } | { description: RTCSessionDescription; from: `0x${string}`; to: `0x${string}` } | { iceCandidate: RTCIceCandidate; from: `0x${string}`; to: `0x${string}` };
-export type PeerConnection = { conn: RTCPeerConnection; channel: RTCDataChannel }
-export type PeerConnections = Record<`0x${string}`, PeerConnection>;
