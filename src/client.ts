78327d74578391d49e1e1211ad091c4cd7db67f9
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-10 18:49:24 +1000

    fix import

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import { start, KeyManager } from '.'
 import coinOracle from '../oracles/oracle_Coin'
 import nameServiceOracle from '../oracles/oracle_NameService'
 import demoOracle from '../oracles/oracle_Demo'
-import theRadicalParty from './oracles/oracle_TheRadicalParty'
+import theRadicalParty from '../oracles/oracle_TheRadicalParty'
 
 const keyManager = new KeyManager('client')
 
