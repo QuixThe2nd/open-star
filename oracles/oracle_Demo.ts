3a21e44706662ee2e3ae7ff664d039cf81ff2420
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-10 18:46:03 +1000

    dont hardcode time

--- oracles/oracle_Demo.ts
+++ oracles/oracle_Demo.ts
@@ -2,18 +2,18 @@ import { StateManager, type Oracle, mode } from ".."
 
 const state = new StateManager({ number: 0 })
 const methods = {
-  add: (args: { value: number, time: number }): string | void => {
+  add: (args: { value: number }): string | void => {
     if (args.value <= 0) return 'Value must be positive'
     state.set({ number: Number(state.value.number) + Number(args.value) })
   },
-  subtract: (args: { value: number, time: number }): string | void => {
+  subtract: (args: { value: number }): string | void => {
     if (args.value <= 0) return 'Value must be positive'
     state.set({ number: state.value.number - args.value })
   }
 }
 const methodDescriptions = {
-  add: { value: 0, time: 0 },
-  subtract: { value: 0, time: 0 },
+  add: { value: 0 },
+  subtract: { value: 0 },
 }
 
 const oracle: Oracle<typeof state.value, typeof methods> = { name: 'DEMO', epochTime: 5_000, state, methods, methodDescriptions, startupState: (peerStates) => mode(peerStates) }
