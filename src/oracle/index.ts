d2cdb2927ccb72adb398952ed5f466f3dc5a3059
Parsa Yazdani (Quix) <parsa@yazdani.au>
2025-05-12 21:23:23 +1000

    ORC1

--- src/oracle/index.ts
+++ src/oracle/index.ts
@@ -1,10 +1,8 @@
-import type { ORC20State } from '..'
+import type { ORC1State, ORC20State } from '..'
 import type { KeyManager } from '../classes/KeyManager'
-import type { MethodReturn, ORC20Oracle, Oracle } from '../types/Oracle'
+import type { ORC1Oracle, ORC20Oracle, Oracle } from '../types/Oracle'
 import { OpenStar } from './OpenStar'
+import { OpenStarRC1 } from './OpenStarRC1'
 import { OpenStarRC20 } from './OpenStarRC20'
 
-export const start = <OracleState extends Record<string, unknown> | ORC20State, OracleMethods extends Record<string, (arg: any) => MethodReturn>, OracleName extends OracleState extends ORC20State ? `ORC20_${string}` : string>(
-	oracle: OracleState extends ORC20State ? ORC20Oracle<OracleMethods, OracleState, Extract<OracleName, `ORC20_${string}`>> : Oracle<OracleMethods, OracleState, OracleName>,
-	keyManager?: KeyManager
-) => ('ORC20' in oracle ? new OpenStarRC20(oracle as ORC20Oracle<OracleMethods>, keyManager) : new OpenStar(oracle, keyManager))
+export const start = (oracle: ORC1Oracle<ORC1State, `ORC1_${string}`> | ORC20Oracle<ORC20State, `ORC20_${string}`> | Oracle, keyManager?: KeyManager) => 'ORC1' in oracle ? new OpenStarRC1(oracle, keyManager) : 'ORC20' in oracle ? new OpenStarRC20(oracle, keyManager) : new OpenStar(oracle, keyManager)
